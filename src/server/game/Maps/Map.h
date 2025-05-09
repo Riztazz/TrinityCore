16576b20e4f74111ff10ac04fa6a7c508bad5a36
ulmetrs <robertulmet@gmail.com>
2025-05-09 17:34:08 -0400

    Update Map.h

--- src/server/game/Maps/Map.h
+++ src/server/game/Maps/Map.h
@@ -305,7 +305,7 @@ class TC_GAME_API Map : public GridRefManager<NGridType>
 {
     friend class MapReference;
     public:
-        Map(uint32 id, uint32 InstanceId, uint8 SpawnMode, Map* _parent = nullptr);
+        Map(uint32 id);
         virtual ~Map();
 
         MapEntry const* GetEntry() const { return i_mapEntry; }
