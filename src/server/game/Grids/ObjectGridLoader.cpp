e4014c630bd11a33c0638edd32c3b556af0556f4
ulmetrs <robertulmet@gmail.com>
2025-05-11 01:35:57 -0400

    Update ObjectGridLoader.cpp

--- src/server/game/Grids/ObjectGridLoader.cpp
+++ src/server/game/Grids/ObjectGridLoader.cpp
@@ -114,17 +114,6 @@ void LoadHelper(CellGuidSet const& guid_set, CellCoord &cell, GridRefManager<T> 
         {
             GameObjectData const* data = sObjectMgr->GetGameObjectData(guid);
             ASSERT(data);
-            // Here we skip objects that should be loaded in a different partition
-            if (sMapMgr->CalculatePartitionId(map->GetId(), data->spawnPoint) != map->GetPartitionId())
-            {
-                TC_LOG_DEBUG("partitions", "Object {} NOT loaded in partition {} ", guid, map->GetPartitionId());
-                continue;
-            }
-            else
-            {
-                TC_LOG_DEBUG("partitions", "Object {} loaded in partition {} ", guid, map->GetPartitionId());
-                continue;
-            }
             obj = (T*)GameObject::CreateGameObject(data->id);
         }
         else
@@ -133,6 +122,17 @@ void LoadHelper(CellGuidSet const& guid_set, CellCoord &cell, GridRefManager<T> 
         if (!obj->LoadFromDB(guid, map, false, false))
         {
             delete obj;
+            continue;
+        }
+        // Here we skip objects that should be loaded in a different partition
+        if (sMapMgr->CalculatePartitionId(map->GetId(), obj->GetGameObjectData()->spawnPoint) != map->GetPartitionId())
+        {
+            TC_LOG_DEBUG("partitions", "Object {} NOT loaded in partition {} ", guid, map->GetPartitionId());
+            continue;
+        }
+        else
+        {
+            TC_LOG_DEBUG("partitions", "Object {} loaded in partition {} ", guid, map->GetPartitionId());
             continue;
         }
         AddObjectHelper(cell, m, count, map, obj);
