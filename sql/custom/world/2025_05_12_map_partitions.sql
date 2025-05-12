17980a2acf56d0299b0bb45599d602fa64525606
ulmetrs <robertulmet@gmail.com>
2025-05-12 12:16:37 -0400

    try this

--- sql/custom/world/2025_05_12_map_partitions.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `map_partitions`  (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `mapId` INT NOT NULL,
-  `partitionId` INT NOT NULL,
-  `polygon` TEXT NOT NULL,
-  PRIMARY KEY (`id`)
-);
