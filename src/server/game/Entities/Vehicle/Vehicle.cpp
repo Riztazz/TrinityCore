712174226254ed2bebf21f693db2e017d6a96aa6
ulmetrs <robertulmet@gmail.com>
2025-05-16 14:29:44 -0400

    Update Vehicle.cpp

--- src/server/game/Entities/Vehicle/Vehicle.cpp
+++ src/server/game/Entities/Vehicle/Vehicle.cpp
@@ -627,7 +627,6 @@ void Vehicle::TeleportPassengers(uint32 mapId, float x, float y, float z, float 
 void Vehicle::UpdatePassengersMapPartition(Map* map)
 {
     TC_LOG_DEBUG("partitions", "Vehicle::UpdatePassengersMapPartition {}", GetDebugInfo());
-    // Not sure if we need to copy the players list, but doing so since TeleportPassengers does
     std::vector<Player*> players;
     std::vector<Creature*> creatures;
     for (SeatMap::const_iterator itr = Seats.begin(); itr != Seats.end(); ++itr)
