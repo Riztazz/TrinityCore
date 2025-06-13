cf1bc47b3c72c046eaf8a257a28789ae93934611
ulmetrs <robertulmet@gmail.com>
2025-06-12 21:59:19 -0400

    Update Creature.cpp

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -1000,7 +1000,7 @@ void Creature::Update(uint32 diff)
             RegenerateAll(diff);
 
             // If we are the default leader but not the current leader attempt to reset the formation
-            if (m_formation && !m_formation->IsLeader(this) && m_formation->GetLeaderSpawnId() == GetSpawnId())
+            if (m_formation && m_formation->GetLeaderSpawnId() == GetSpawnId() && !IsFormationLeader())
                 m_formation->FormationReset();
 
             break;
