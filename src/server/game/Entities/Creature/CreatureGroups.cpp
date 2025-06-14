f2c99396509a1b8de3d8fec5af7c40b304825ef4
ulmetrs <robertulmet@gmail.com>
2025-06-14 14:09:56 -0400

    try re-engage

--- src/server/game/Entities/Creature/CreatureGroups.cpp
+++ src/server/game/Entities/Creature/CreatureGroups.cpp
@@ -267,8 +267,19 @@ void CreatureGroup::RemoveMember(Creature* member)
     // Copy old leaders waypoint info
     if (_leaderPathId)
     {
+        Unit* engagedTarget = nullptr;
+        if (newLeader->IsEngaged())
+            engagedTarget = newLeader->GetThreatManager()->GetCurrentVictim();
+
         newLeader->UpdateCurrentWaypointInfo(_leader->GetCurrentWaypointInfo().first, _leader->GetCurrentWaypointInfo().second);
         newLeader->GetMotionMaster()->MovePath(_leaderPathId, true);
+
+        // Reengage if needed
+        if (engagedTarget)
+        {
+            TC_LOG_DEBUG("formations", "CreatureGroup::RemoveMember {} New leader engaging target {}", _leaderSpawnId, engagedTarget->GetGUID().ToString());
+            newLeader->EngageWithTarget(engagedTarget);
+        }
     }
 
     // set new leader
