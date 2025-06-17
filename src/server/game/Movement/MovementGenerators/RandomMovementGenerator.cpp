cc47617d53909f96eacd6ca59ed9ffac7bfded35
ulmetrs <robertulmet@gmail.com>
2025-06-16 21:06:31 -0400

    Update RandomMovementGenerator.cpp

--- src/server/game/Movement/MovementGenerators/RandomMovementGenerator.cpp
+++ src/server/game/Movement/MovementGenerators/RandomMovementGenerator.cpp
@@ -248,8 +248,8 @@ void RandomMovementGenerator<Creature>::SetRandomLocation(Creature* owner)
             TC_LOG_DEBUG("smooth", "creating subsequent path length: {}", PathGenerator::ComputePathLength(_paths[_pathIndex]));
             TC_LOG_DEBUG("smooth", "creating subsequent path double truncated length: {}", PathGenerator::ComputePathLength(modPath));
         }
-        modPath.insert(modPath.begin(), PositionToVector3(owner->GetPosition()));
         modPath.insert(modPath.begin(), PositionToVector3(_paths[_pathIndex].front()));
+        modPath.insert(modPath.begin(), PositionToVector3(owner->GetPosition()));
         if (owner->GetSpawnId() == 80043)
         {
             TC_LOG_DEBUG("smooth", "creating subsequent path truncated length with owner position and path start prepended: {}", PathGenerator::ComputePathLength(modPath));
