0f1af0a34b84ca705a5392bb024a01901a684210
ulmetrs <robertulmet@gmail.com>
2025-06-18 18:16:52 -0400

    Update WaypointMovementGenerator.h

--- src/server/game/Movement/MovementGenerators/WaypointMovementGenerator.h
+++ src/server/game/Movement/MovementGenerators/WaypointMovementGenerator.h
@@ -19,6 +19,7 @@
 #define TRINITY_WAYPOINTMOVEMENTGENERATOR_H
 
 #include "MovementGenerator.h"
+#include "MoveSplineInitArgs.h"
 #include "PathMovementBase.h"
 #include "Position.h"
 #include "Timer.h"
