e74f79ac38802bb7aa61b7acc0989d4a13a73933
ulmetrs <robertulmet@gmail.com>
2025-06-20 16:29:46 -0400

    make const

--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -727,7 +727,7 @@ void Map::UpdatePlayerZoneStats(uint32 oldZone, uint32 newZone)
     ++_zonePlayerCountMap[newZone];
 }
 
-uint32 Map::GetZonePlayerCount(uint32 zoneId) {
+uint32 Map::GetZonePlayerCount(uint32 zoneId) const {
     auto it = _zonePlayerCountMap.find(zoneId);
     if (it == _zonePlayerCountMap.end())
         return 0;
--- src/server/game/Maps/Map.h
+++ src/server/game/Maps/Map.h
@@ -435,7 +435,7 @@ class TC_GAME_API Map : public GridRefManager<NGridType>
         virtual GridMap* GetGrid(float x, float y);
         virtual void UpdateWeather(uint32 t_diff);
         virtual void UpdatePlayerZoneStats(uint32 oldZone, uint32 newZone);
-        virtual uint32 GetZonePlayerCount(uint32 zoneId);
+        virtual uint32 GetZonePlayerCount(uint32 zoneId) const;
 
         static bool ExistMap(uint32 mapId, int gx, int gy);
         static bool ExistVMap(uint32 mapId, int gx, int gy);
@@ -968,7 +968,7 @@ class TC_GAME_API PartitionMap : public Map
         GridMap* GetGrid(float x, float y) override { return _parent->GetGrid(x, y); }
         void UpdateWeather(uint32 t_diff) override { /* do nothing, parent updates weather */ }
         void UpdatePlayerZoneStats(uint32 oldZone, uint32 newZone) override { _parent->UpdatePlayerZoneStats(oldZone, newZone); }
-        uint32 GetZonePlayerCount(uint32 zoneId) override { return _parent->GetZonePlayerCount(zoneId); }
+        uint32 GetZonePlayerCount(uint32 zoneId) const override { return _parent->GetZonePlayerCount(zoneId); }
 
         void SendZoneDynamicInfo(uint32 zoneId, Player* player) const override
         {
