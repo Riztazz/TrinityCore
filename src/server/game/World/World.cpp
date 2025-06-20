04106ee8ea63ee1d120306fa51c63962e6a4d95d
ulmetrs <robertulmet@gmail.com>
2025-06-20 12:47:01 -0400

    Update World.cpp

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -2348,7 +2348,7 @@ void World::SetInitialWorldSettings()
         {
             if (!map->Instanceable())
             {
-                TC_LOG_INFO("server.loading", "Pre-loading base map data for map {} partition {}", map->GetId(), map->GetPartition());
+                TC_LOG_INFO("server.loading", "Pre-loading base map data for map {} partition {}", map->GetId(), map->GetPartitionId());
                 map->LoadAllCells();
             }
         });
