5443741daa66aed115185e2530b9b860e8635715
ulmetrs <robertulmet@gmail.com>
2025-06-23 10:43:50 -0400

    delay teleport for diff partitions

--- src/server/game/Entities/Transport/Transport.h
+++ src/server/game/Entities/Transport/Transport.h
@@ -145,7 +145,7 @@ class TC_GAME_API Transport : public GenericTransport
     private:
         void MoveToNextWaypoint();
         float CalculateSegmentPos(float perc);
-        bool TeleportTransport(uint32 newMapid, float x, float y, float z, float o);
+        bool TeleportTransport();
         void DelayedTeleportTransport();
         void DoEventIfAny(KeyFrame const& node, bool departure);
 
