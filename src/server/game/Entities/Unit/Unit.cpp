fd1bc0ec6da411d056b559550ce08064c896629e
ulmetrs <robertulmet@gmail.com>
2025-06-30 09:48:32 -0400

    skip notifies for now

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -465,97 +465,110 @@ void Unit::Update(uint32 p_time)
 {
     ZoneScopedN("Unit::Update")
 
-    // @tswow-begin
     {
-        ZoneScopedNC("TSUnit::Tick", MAP_UPDATE_COLOR);
+        ZoneScopedN("Unit::Update::A")
 
-        m_tsWorldEntity.tick(TSWorldObject(this));
+        // @tswow-begin
+        {
+            ZoneScopedNC("TSUnit::Tick", MAP_UPDATE_COLOR);
+
+            m_tsWorldEntity.tick(TSWorldObject(this));
+        }
+        
+        {
+            ZoneScopedNC("TSUnit::CollisionsTick", MAP_UPDATE_COLOR);
+
+            m_tsCollisions.Tick(TSWorldObject(this));
+        }
+        // @tswow-end
+
+        // WARNING! Order of execution here is important, do not change.
+        // Spells must be processed with event system BEFORE they go to _UpdateSpells.
+        // Or else we may have some SPELL_STATE_FINISHED spells stalled in pointers, that is bad.
+        {
+            ZoneScopedN("Unit::Update::EventsUpdate")
+
+            m_Events.Update(p_time);
+        }
+
+        CheckPendingMovementAcks();
+
+        if (!IsInWorld())
+            return;
+
+        _UpdateSpells(p_time);
+
+        // If this is set during update SetCantProc(false) call is missing somewhere in the code
+        // Having this would prevent spells from being proced, so let's crash
+        ASSERT(!m_procDeep);
+
+        m_combatManager.Update(p_time);
+
+        _lastDamagedTargetGuid = ObjectGuid::Empty;
+        if (_lastExtraAttackSpell)
+        {
+            ZoneScopedN("Unit::Update::ExtraAttacks")
+
+            while (!extraAttacksTargets.empty())
+            {
+                auto itr = extraAttacksTargets.begin();
+                ObjectGuid targetGuid = itr->first;
+                uint32 count = itr->second;
+                extraAttacksTargets.erase(itr);
+                if (Unit* victim = ObjectAccessor::GetUnit(*this, targetGuid))
+                    if (victim->IsWithinMeleeRange(this))
+                        HandleProcExtraAttackFor(victim, count);
+            }
+            _lastExtraAttackSpell = 0;
+        }
+
+        // not implemented before 3.0.2
+        if (uint32 base_att = getAttackTimer(BASE_ATTACK))
+            setAttackTimer(BASE_ATTACK, (p_time >= base_att ? 0 : base_att - p_time));
+        if (uint32 ranged_att = getAttackTimer(RANGED_ATTACK))
+            setAttackTimer(RANGED_ATTACK, (p_time >= ranged_att ? 0 : ranged_att - p_time));
+        if (uint32 off_att = getAttackTimer(OFF_ATTACK))
+            setAttackTimer(OFF_ATTACK, (p_time >= off_att ? 0 : off_att - p_time));
+
+        // update abilities available only for fraction of time
+        UpdateReactives(p_time);
+
+        if (IsAlive())
+        {
+            ZoneScopedN("Unit::Update::AuraStates")
+
+            ModifyAuraState(AURA_STATE_HEALTHLESS_20_PERCENT, HealthBelowPct(20));
+            ModifyAuraState(AURA_STATE_HEALTHLESS_35_PERCENT, HealthBelowPct(35));
+            ModifyAuraState(AURA_STATE_HEALTH_ABOVE_75_PERCENT, HealthAbovePct(75));
+        }
+    }
+
+    {
+        ZoneScopedN("Unit::Update::B")
+
+        UpdateSplineMovement(p_time);
+        i_motionMaster->Update(p_time);
     }
     
+
     {
-        ZoneScopedNC("TSUnit::CollisionsTick", MAP_UPDATE_COLOR);
+        ZoneScopedN("Unit::Update::C")
 
-        m_tsCollisions.Tick(TSWorldObject(this));
+        // Wait with the aura interrupts until we have updated our movement generators and position
+        if (GetTypeId() == TYPEID_PLAYER)
+            InterruptMovementBasedAuras();
+        else if (!movespline->Finalized())
+            InterruptMovementBasedAuras();
+
+        // All position info based actions have been executed, reset info
+        _positionUpdateInfo.Reset();
+
+        if (HasScheduledAIChange() && (GetTypeId() != TYPEID_PLAYER || (IsCharmed() && GetCharmerGUID().IsCreature())))
+            UpdateCharmAI();
+        RefreshAI();
+
+        InvalidateValuesUpdateCache();
     }
-    // @tswow-end
-
-    // WARNING! Order of execution here is important, do not change.
-    // Spells must be processed with event system BEFORE they go to _UpdateSpells.
-    // Or else we may have some SPELL_STATE_FINISHED spells stalled in pointers, that is bad.
-    {
-        ZoneScopedN("Unit::Update::EventsUpdate")
-
-        m_Events.Update(p_time);
-    }
-
-    CheckPendingMovementAcks();
-
-    if (!IsInWorld())
-        return;
-
-    _UpdateSpells(p_time);
-
-    // If this is set during update SetCantProc(false) call is missing somewhere in the code
-    // Having this would prevent spells from being proced, so let's crash
-    ASSERT(!m_procDeep);
-
-    m_combatManager.Update(p_time);
-
-    _lastDamagedTargetGuid = ObjectGuid::Empty;
-    if (_lastExtraAttackSpell)
-    {
-        ZoneScopedN("Unit::Update::ExtraAttacks")
-
-        while (!extraAttacksTargets.empty())
-        {
-            auto itr = extraAttacksTargets.begin();
-            ObjectGuid targetGuid = itr->first;
-            uint32 count = itr->second;
-            extraAttacksTargets.erase(itr);
-            if (Unit* victim = ObjectAccessor::GetUnit(*this, targetGuid))
-                if (victim->IsWithinMeleeRange(this))
-                    HandleProcExtraAttackFor(victim, count);
-        }
-        _lastExtraAttackSpell = 0;
-    }
-
-    // not implemented before 3.0.2
-    if (uint32 base_att = getAttackTimer(BASE_ATTACK))
-        setAttackTimer(BASE_ATTACK, (p_time >= base_att ? 0 : base_att - p_time));
-    if (uint32 ranged_att = getAttackTimer(RANGED_ATTACK))
-        setAttackTimer(RANGED_ATTACK, (p_time >= ranged_att ? 0 : ranged_att - p_time));
-    if (uint32 off_att = getAttackTimer(OFF_ATTACK))
-        setAttackTimer(OFF_ATTACK, (p_time >= off_att ? 0 : off_att - p_time));
-
-    // update abilities available only for fraction of time
-    UpdateReactives(p_time);
-
-    if (IsAlive())
-    {
-        ZoneScopedN("Unit::Update::AuraStates")
-
-        ModifyAuraState(AURA_STATE_HEALTHLESS_20_PERCENT, HealthBelowPct(20));
-        ModifyAuraState(AURA_STATE_HEALTHLESS_35_PERCENT, HealthBelowPct(35));
-        ModifyAuraState(AURA_STATE_HEALTH_ABOVE_75_PERCENT, HealthAbovePct(75));
-    }
-
-    UpdateSplineMovement(p_time);
-    i_motionMaster->Update(p_time);
-
-    // Wait with the aura interrupts until we have updated our movement generators and position
-    if (GetTypeId() == TYPEID_PLAYER)
-        InterruptMovementBasedAuras();
-    else if (!movespline->Finalized())
-        InterruptMovementBasedAuras();
-
-    // All position info based actions have been executed, reset info
-    _positionUpdateInfo.Reset();
-
-    if (HasScheduledAIChange() && (GetTypeId() != TYPEID_PLAYER || (IsCharmed() && GetCharmerGUID().IsCreature())))
-        UpdateCharmAI();
-    RefreshAI();
-
-    InvalidateValuesUpdateCache();
 }
 
 bool Unit::haveOffhandWeapon() const
--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -1170,10 +1170,10 @@ void Map::ProcessRelocationNotifies(const uint32 diff)
 
         for (GridRefManager<NGridType>::iterator i = GridRefManager<NGridType>::begin(); i != GridRefManager<NGridType>::end(); ++i)
         {
-            NGridType *grid = i->GetSource();
-            uint32 gx = grid->getX(), gy = grid->getY();
-            uint32 grid_id = gy * MAX_NUMBER_OF_GRIDS + gx;
-            if (!isGridMarked(grid_id))
+            //NGridType *grid = i->GetSource();
+            ////uint32 gx = grid->getX(), gy = grid->getY();
+            //uint32 grid_id = gy * MAX_NUMBER_OF_GRIDS + gx;
+            //if (!isGridMarked(grid_id))
                 continue;
 
             grid->getRelocationTimer().TUpdate(diff);
@@ -1218,10 +1218,10 @@ void Map::ProcessRelocationNotifies(const uint32 diff)
         TypeContainerVisitor<ResetNotifier, WorldTypeMapContainer > world_notifier(reset);
         for (GridRefManager<NGridType>::iterator i = GridRefManager<NGridType>::begin(); i != GridRefManager<NGridType>::end(); ++i)
         {
-            NGridType *grid = i->GetSource();
-            uint32 gx = grid->getX(), gy = grid->getY();
-            uint32 grid_id = gy * MAX_NUMBER_OF_GRIDS + gx;
-            if (!isGridMarked(grid_id))
+            //NGridType *grid = i->GetSource();
+            //uint32 gx = grid->getX(), gy = grid->getY();
+            //uint32 grid_id = gy * MAX_NUMBER_OF_GRIDS + gx;
+            //if (!isGridMarked(grid_id))
                 continue;
 
             if (!grid->getRelocationTimer().TPassed())
