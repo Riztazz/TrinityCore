6dd93d511b9a886286df8817a02af32904678ba3
ulmetrs <robertulmet@gmail.com>
2025-07-26 13:47:53 -0400

    move to auth

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -362,7 +362,6 @@ void World::AddSession_(WorldSession* s)
 
     uint32 Sessions = GetActiveAndQueuedSessionCount();
     uint32 pLimit = GetPlayerAmountLimit();
-    bool pLimitNoQueue = GetPlayerAmountLimitNoQueue();
     uint32 QueueSize = GetQueuedSessionCount(); //number of players in the queue
 
     //so we don't count the user trying to
@@ -372,13 +371,6 @@ void World::AddSession_(WorldSession* s)
 
     if (pLimit > 0 && Sessions >= pLimit && !s->HasPermission(rbac::RBAC_PERM_SKIP_QUEUE) && !HasRecentlyDisconnected(s))
     {
-        if (pLimitNoQueue)
-        {
-            s->KickPlayer("World::AddSession_ Player Limit Reached with NoQueue set");
-            delete s;                                           // session not added yet in session list, so not listed in queue
-            return;
-        }
-
         AddQueuedPlayer(s);
         UpdateMaxSessionCounters();
         TC_LOG_INFO("misc", "PlayerQueue: Account id {} is in Queue Position ({}).", s->GetAccountId(), ++QueueSize);
