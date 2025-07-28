e512f2bd053810a082326bd5f4237f04b5b64864
ulmetrs <robertulmet@gmail.com>
2025-07-28 17:51:59 -0400

    Update Map.cpp

--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -270,15 +270,6 @@ void Map::LoadAllCells()
 
 void AddUpdateObject(Object* obj)
 {
-    auto currentThread = std::this_thread::get_id();
-    auto ownerThread = _updateObjectsOwnerThread.load();
-    
-    if (ownerThread != std::thread::id{} && currentThread != ownerThread)
-    {
-        TC_LOG_ERROR("concurrency", "Cross-thread AddUpdateObject detected! Owner: 0x{:x} Current: 0x{:x}", 
-                        std::hash<std::thread::id>{}(ownerThread), std::hash<std::thread::id>{}(currentThread));
-    }
-    
     if (m_processingObjectUpdates)
         return;
     std::lock_guard<std::mutex> lock(m_processingObjectUpdatesLock);
