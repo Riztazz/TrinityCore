ead3d27e1b3d1342d07980eef452c3eceb956129
ulmetrs <robertulmet@gmail.com>
2025-07-29 10:32:54 -0400

    add chest level check and set to locked

--- src/server/game/Entities/GameObject/GameObject.cpp
+++ src/server/game/Entities/GameObject/GameObject.cpp
@@ -2861,7 +2861,7 @@ void GameObject::BuildValuesUpdate(uint8 updateType, ByteBuffer* data, Player* t
             {
                 uint32 goFlags = m_uint32Values[GAMEOBJECT_FLAGS];
                 if (GetGoType() == GAMEOBJECT_TYPE_CHEST)
-                    if (GetGOInfo()->chest.groupLootRules && !IsLootAllowedFor(target))
+                    if ((GetGOInfo()->chest.groupLootRules && !IsLootAllowedFor(target)) || GetGOInfo()->chest.level > target->GetLevel())
                         goFlags |= GO_FLAG_LOCKED | GO_FLAG_NOT_SELECTABLE;
 
                 fieldBuffer << goFlags;
